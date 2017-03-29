3d0b2e571eac0a147c61c74c6d27265a0b948256
Aliaksei Parkhimovich <aliaksei_parkhimovich@epam.com>
2017-03-29 12:19:15 +0300

    project

---
+++ Volotea/Volotea/Steps/PaymentStepPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System.Threading;
+using Volotea.Utils;
+
+namespace Volotea.Steps
+{
+    public class PaymentStepPage : BasePage
+    {
+        private string flexWindowXPath = "//a[@id = 'closeFlexLink']";
+        private string cvvXPath = "//input[@id = 'cvv1']";
+        private string cvv = "123";
+        private string bookThisFlightButtonXPath = "//a[@id = 'ControlGroupPaymentNewBottom_LinkButtonSubmit']";
+
+        public PaymentStepPage(BasePage bp) : base(bp)
+        {
+        }
+
+        public WaitingPage BookThisFlight(BasePage bp)
+        {
+
+            //there is popup window on page, i can not disable it or change driver focus to web page from popup window
+
+            Thread.Sleep(500);
+            WebElementHelper.WaitAndClick(Driver, By.XPath(flexWindowXPath));
+            WebElementHelper.WaitAndSendKeys(Driver, By.XPath(cvvXPath), cvv);
+            WebElementHelper.WaitAndClick(Driver, By.XPath(bookThisFlightButtonXPath));
+            return new WaitingPage(bp);
+        }
+    }
+}
---
+++ Volotea/Volotea/Tests/TicketBookingTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Threading;
+using Volotea.Steps;
+
+namespace Volotea.Tests
+{
+    [TestFixture]
+    public class TicketBookingTest : BaseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            page = new Steps.LoginElement("chrome");
+        }
+
+        [Test]
+        public void TicketBookingCheck()
+        {
+            page.GoTo(Data.TestData.BaseURL);
+            MainPage mp = page.SignIn(page, Data.TestData.Login, Data.TestData.Password);
+            BookingFirstStepPage bfsp = mp.SelectFlight(mp);
+            BookingSecondStepPage bssp = bfsp.NextStep(bfsp);
+            BookingThirdStepPage btsp = bssp.NextStep(bssp);
+            BookingForthStepPage bForthSP = btsp.ContinueAndCustomize(btsp);
+            PaymentStepPage psp = bForthSP.NextStep(bForthSP);
+            WaitingPage wp = psp.BookThisFlight(psp);
+            Assert.True(wp.IsPaymentPass(wp));
+        }
+    }
+}
