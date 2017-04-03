a193ca989cb901ed9082721c58d3e990c98c5610
Aliaksei_Parkhimovich <Aliaksei_Parkhimovich@epam.com>
2017-04-03 15:19:31 +0300

    changes in TwoTabsLogin

--- Volotea/Volotea/Steps/LoginElement.cs
+++ Volotea/Volotea/Steps/LoginElement.cs
@@ -56,10 +56,19 @@ namespace Volotea.Steps
             SignInElement.Click();
             this.WaitUntilDisplayed(UsernameFieldElement);
             this.WaitUntilDisplayed(UserpasswordElement);
+            this.FillFields(username, password);
+            RememberLoginElement.Click();
+            LoginButtonElement.Click();
+
+            /*
+            SignInElement.Click();
+            this.WaitUntilDisplayed(UsernameFieldElement);
+            this.WaitUntilDisplayed(UserpasswordElement);
 
             this.FillFields(username, password);
             RememberLoginElement.Click();
             LoginButtonElement.Click();
+            */
         }
 
         private void FillFields(string username, string password)
